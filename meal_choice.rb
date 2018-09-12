bf512be4de9a159b5f8674a02ea883325e4ea530
Scott M <gmat191@gmail.com>
2018-09-12 00:05:54 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,8 +1,9 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
-def meal_choice(arg1 = "meat")
-  arg1
+def meal_choice(arg1 = "meat") #we define our method and set the default argument
+  arg1 #return the default argument
 end
-meal_choice
+meal_choice #call our method: the result will be "meat"
 
 meal_choice("vegan")
+#dynamically change our method by setting our name equal to "vegan"
